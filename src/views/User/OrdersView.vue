4377404ae7c8dd1f8fdf67b2c832e4ca1d651899
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2025-04-16 17:58:49 +0300

    6.3

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -7,7 +7,8 @@
                     <v-list-item v-for="order in orders" :key="order.id">
                         <template v-slot:prepend>
                             <v-list-item-action>
-                                <v-checkbox :input-value="order.done" color="primary"></v-checkbox>
+                                <v-checkbox :input-value="order.done" color="primary"
+                                    @click="markDone(order)"></v-checkbox>
                             </v-list-item-action>
                         </template>
                         <v-list-item-title>{{ order.name }}</v-list-item-title>
@@ -40,8 +41,14 @@ export default {
                     done: true
                 }
             ]
-
         }
     },
+    methods: {
+        markDone(order) {
+            order.done = !order.done;
+            console.log(order.done);
+        },
+    },
+
 }
 </script>
