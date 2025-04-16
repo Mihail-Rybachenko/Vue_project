70fca07ccb9491413e1b260ee92697900522f44b
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2025-04-16 18:01:41 +0300

    7.2

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -48,13 +48,23 @@
 export default {
     data() {
         return {
-            title: "...",
-            desc: "...",
-            promo: true,
-            src: "...",
-            id: ""
+            valid: false,
+            title: "",
+            description: "",
+            promo: true
         }
-
+    },
+    methods: {
+        createAd() {
+            if (this.$refs.form.validate()) {
+                const ad = {
+                    title: this.title,
+                    desc: this.description,
+                    promo: this.promo
+                }
+                console.log(ad)
+            }
+        }
     }
 }
 </script>
